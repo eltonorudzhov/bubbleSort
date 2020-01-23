d0f22bacfc4a04ccda731791d0583dbdaf9e4960
eltonorudzhov <56114594+eltonorudzhov@users.noreply.github.com>
2020-01-23 23:52:55 +0300

    upd index.js
    интервал изменен на таймер

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import generate from "./src/js/generate.js";
 import getRandom from "./src/js/random.js";
 // объявления
 let inputsValues = generate();
-let obj = [];
 let trDiv = document.createElement("tr");
 let w = 1200 / inputsValues.count;
 for (let i = 0; i < inputsValues.count; i++) {
   let elemNumber = getRandom(inputsValues.min, inputsValues.max);
   let td = document.createElement("td");
-
-  td.style.width = w + "px";
+  td.style.textAlign = td.style.width = w + "px";
+  td.style.fontSize = w / 2 + "px";
   td.id = "td" + i;
   td.style.height = w + "px";
   trDiv.appendChild(td);
@@ -20,44 +19,41 @@ document.getElementById("sortElements").append(trDiv);
 sortBubble();
 
 function sortBubble() {
-  if (inputsValues.count==1){
-    return
+  if (inputsValues.count == 1) {
+    return;
   }
   let i = inputsValues.count - 1;
-  document.getElementById("td0").setAttribute("class", "checker");
-  timer(i)
-  i--;
-  let timerId = setInterval(function() {
-    document.getElementById("td0").setAttribute("class", "checker");
-    console.log(i)
-    if (i==0) {
-      console.log("ААА")
-      clearInterval(timerId);
-      
+  // timer(i)
+  let interv;
+
+  setTimeout(function run() {
+    interv = (i + 3) * 300;
+    timer(i);
+    if (i > 1) {
+      setTimeout(run, interv);
+    } else {
+      console.log(i);
+      console.log("DDD");
       document.getElementById("td0").setAttribute("class", "simple");
       document.getElementById("td1").setAttribute("class", "simple");
-      return
+      return;
     }
-    timer(i)
-
-    document.getElementById("td" + (i + 1)).setAttribute("class", "simple");
     i--;
-  }, 400*(i+3));
- 
+  }, interv);
 }
 
-
-function timer(i){
-  let j = 0
-  
-  console.log(i, j)
+function timer(i) {
+  let j = 0;
+  document.getElementById("td0").setAttribute("class", "checker");
+  console.log(i, j);
   let firstTimer = setInterval(function() {
-    if ((j==i-1)) {
+    if (j == i - 1) {
+      document.getElementById("td" + j).setAttribute("class", "simple");
       clearInterval(firstTimer);
     }
     check(j);
     j++;
-  }, 400);
+  }, 300);
 }
 
 function check(j) {
@@ -67,14 +63,12 @@ function check(j) {
   ) {
     change(j);
   } else {
-    setTimeout(() =>{
-      console.log("хы")
-       document.getElementById("td" + (j + 1)).setAttribute("class", "checker")
-      },50
-       
-       );
-       
-    document.getElementById("td" + (j + 1)).setAttribute("class", "checked")
+    setTimeout(() => {
+      console.log("хы");
+      document.getElementById("td" + (j + 1)).setAttribute("class", "checker");
+    }, 50);
+
+    document.getElementById("td" + (j + 1)).setAttribute("class", "checked");
     document.getElementById("td" + j).setAttribute("class", "simple");
   }
 }
@@ -87,5 +81,4 @@ function change(j) {
     );
   document.getElementById("td" + j).id = "td" + (j + 1);
   document.getElementById("td" + (j + 1)).id = "td" + j;
-
 }
