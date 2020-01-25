e3f714d81830f949b28936eaa07c2dea12c32ba8
eltonorudzhov <56114594+eltonorudzhov@users.noreply.github.com>
2020-01-25 12:18:41 +0300

    конечный вариант

--- index.js
+++ index.js
@@ -1,90 +1,54 @@
 import generate from "./src/js/generate.js";
-import getRandom from "./src/js/random.js";
+import changeClass from './src/js/changeclass.js'
+import checkElements from './src/js/checkelements.js'
+import buttonsAbled from './src/js/buttonsabled.js'
+import checkNeeds from './src/js/checkneed.js'
 // объявления
-let inputsValues = generate();
-let trDiv = document.createElement("tr");
+let inputsValues=generate()
 let speed = (13-inputsValues.speed)*100
-let w = 1200 / inputsValues.count;
-
-for (let i = 0; i < inputsValues.count; i++) {
-  let elemNumber = getRandom(inputsValues.min, inputsValues.max);
-  let td = document.createElement("td");
-  if (w>400)
-    w=400
-  td.style.textAlign = td.style.width = w + "px";
-  td.style.fontSize = w / 2 + "px";
-  td.id = "td" + i;
-  td.style.height = w + "px";
-  trDiv.appendChild(td);
-  td.setAttribute("class", 'simple');
-  td.innerHTML = +elemNumber;
-}
-document.getElementById("sortElements").append(trDiv);
-
-sortBubble();
+generateButton.onclick=()=> inputsValues = generate();
+startButton.onclick = ()=>  sortBubble()
 
 function sortBubble() {
-  if (inputsValues.count == 1) {
-    return;
-  }
-  let i = inputsValues.count - 1;
-  let interv;
-  setTimeout(function run() {
-    console.log(speed)
-    interv = (i + 3) * speed;
-    timer(i);
-    if (i > 1) {
-      setTimeout(run, interv);
-    } else {
-      changeClass(0, "simple");
-      changeClass(1, "simple");
+   // делаем кнопки не активными, чтобы пользователь мог насладиться анимацией
+   buttonsAbled(true)
+   // если один элемент - сортировка не имеет смысла
+   if (inputsValues.count == 1) {
       return;
-    }
-    i--;
-  }, interv);
-}
-function changeClass(id, className) {
-  document.getElementById("td" + id).setAttribute("class", className);
-}
-function timer(i) {
-  let j = 0;
-  changeClass(0, "checker");
-  //document.getElementById("td0").setAttribute("class", "checker");
-  console.log(i, j);
-  let firstTimer = setInterval(function() {
-    if (j == i - 1) {
-      changeClass(j, "simple");
-      //   document.getElementById("td" + j).setAttribute("class", "simple");
-      clearInterval(firstTimer);
-    }
-    check(i, j);
-    j++;
-  }, speed);
+   }
+   let lastElem = inputsValues.count - 1;
+   let period;
+   setTimeout(function run() {
+     console.log(speed)
+     // задаем период таймера
+      period = (lastElem + 2) * speed;
+      // запускаем интервал
+      oneSeries(lastElem);
+      // проверка необходимости дальнейшей сортировки
+      if ((lastElem > 1)&&(checkNeeds(lastElem))) {
+     
+         setTimeout(run, period);
+      } 
+      else {
+        // активируем кнопки, когда заканчиваем сортирвку
+         buttonsAbled(false)
+         return;
+      }
+   lastElem--;
+   }, period);
 }
 
-function check(i, j) {
-  if (
-    +document.getElementById("td" + j).textContent >
-    +document.getElementById("td" + (j + 1)).textContent
-  ) {
-    change(j);
-  } else {
-    setTimeout(() => {
-      console.log("хы");
-      if (j + 2 <= i) changeClass(j + 1, "checker");
-      else changeClass(j + 1, "simple");
-    }, 50);
-    if (j + 2 <= i) changeClass(j + 1, "checked");
-    changeClass(j, "simple");
-  }
-}
-function change(j) {
-  document
-    .getElementById("td" + j)
-    .parentNode.insertBefore(
-      document.getElementById("td" + (j + 1)),
-      document.getElementById("td" + j)
-    );
-  document.getElementById("td" + j).id = "td" + (j + 1);
-  document.getElementById("td" + (j + 1)).id = "td" + j;
+// Одна серия прохода по массиву
+function oneSeries(lastElem) {
+   let elem = 0;
+   changeClass(0, "checker");
+   let firstTimer = setInterval(function() {
+      if (elem == lastElem - 1) {
+         // Перед выходом из интервала делаем последний элемент простым, а не активным
+         changeClass(elem, "simple");
+         clearInterval(firstTimer);
+      }
+      checkElements(lastElem, elem);
+      elem++;
+   }, speed);
 }
--- src/js/generate.js
+++ src/js/generate.js
@@ -1,17 +1,18 @@
+import createElements from './createElements.js'
+import getInput from './getinput.js'
 export default function generate(){
-    let inputsValues={
-        max: '',
-        min: '',
-        count: '',
-        speed: ''
-    }
-    let element = document.getElementById("MaxValue");
-    inputsValues.max = +element.value
-    element = document.getElementById("MinValue");
-    inputsValues.min = +element.value
-    element = document.getElementById("ItemNumber");
-    inputsValues.count = +element.value
-    element = document.getElementById("Speed");
-    inputsValues.speed = +element.value
-    return inputsValues
+   let inputsValues={
+      max: getInput("maxValue"),
+      min: getInput("minValue"),
+      count: getInput("itemNumber"),
+      speed: getInput("speed")
+   }
+   // если минимальный и максимальный элемент перепутаны - поменять местами
+   if (inputsValues.min>inputsValues.max){
+       document.getElementById("maxValue").value=inputsValues.min
+       document.getElementById("minValue").value=inputsValues.max
+   }
+   // создаем элементы на поле
+   createElements(inputsValues)
+   return inputsValues
 }
